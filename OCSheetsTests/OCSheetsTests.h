2aea51a9d7bafd1ecb8de8c0c89a151edf7f65f9
Benjamin Pilcher <ben.pilcher@tackmobile.com>
2013-05-02 11:28:04 -0600

    Initial Commit

---
+++ OCSheetsTests/OCSheetsTests.h
@@ -0,0 +1,13 @@
+//
+//  OCSheetsTests.h
+//  OCSheetsTests
+//
+//  Created by Ben Pilcher on 5/2/13.
+//  Copyright (c) 2013 Pearson Education. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface OCSheetsTests : SenTestCase
+
+@end
---
+++ OCSheetsTests/OCSheetsTests.m
@@ -0,0 +1,32 @@
+//
+//  OCSheetsTests.m
+//  OCSheetsTests
+//
+//  Created by Ben Pilcher on 5/2/13.
+//  Copyright (c) 2013 Pearson Education. All rights reserved.
+//
+
+#import "OCSheetsTests.h"
+
+@implementation OCSheetsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in OCSheetsTests");
+}
+
+@end
